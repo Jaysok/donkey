84f1b8757066f90527b5e1afd66d3cfcf615e6db
Jaysok <wooljs@gmail.com>
2022-05-17 23:08:50 +0900

    first commit

---
+++ test_deps.ts
@@ -0,0 +1,7 @@
+export * as log from "https://deno.land/std@0.139.0/log/mod.ts";
+export {
+  assert,
+  assertEquals,
+  assertExists,
+  fail,
+} from "https://deno.land/std@0.139.0/testing/asserts.ts";
